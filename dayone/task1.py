12309cefc047997ee3f8933579339f540a0120dc
Dan Mason <psydm3@nottingham.ac.uk>
2017-12-01 16:57:03 +0000

    Complete task1

--- dayone/task1.py
+++ dayone/task1.py
@@ -1,1 +1,12 @@
-captcha = open("input.txt", "r");
+file = open("input.txt", "r")
+captcha = list(file.read())
+total = 0
+
+for i in range(len(captcha)-1):
+    if captcha[i] == captcha[i+1]:
+        total+=int(captcha[i])
+
+if(captcha[len(captcha)-1] == captcha[0]):
+    total+= int(captcha[len(captcha)-1])
+    
+print(total)
