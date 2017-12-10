59429682e1c980c17fbd5cec359dc684e1ab577b
Dan Mason <psydm3@nottingham.ac.uk>
2017-12-10 02:17:32 +0000

    Added input and solutions for day 9

---
+++ daynine/task17.py
@@ -0,0 +1,26 @@
+file = open("input.txt","r")
+stream = file.read()
+read_values = True
+ignore_value = False
+score = 0
+bracket_score = 0
+garbage_value = 0
+
+for x in stream:
+    if read_values:
+        if x == "<":
+            read_values = False
+        elif x == "{":
+            bracket_score+=1
+        elif x == "}":
+            score += bracket_score
+            bracket_score -= 1
+    else:
+        if ignore_value:
+            ignore_value = False
+        else:
+            if x == ">":
+                read_values = True
+            elif x == "!":
+                ignore_value = True
+print(score)
